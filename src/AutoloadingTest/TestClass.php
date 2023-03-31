f28d6aadff6c5b58225fa822fbffde2b244dfed1
Yavor Mihaylov <myavor@vmware.com>
2023-03-31 15:06:32 +0300

    First proof of concept iteration

--- src/AutoloadingTest/TestClass.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\AutoloadingTest;
-
-class TestClass {
-    public function __construct()
-    {
-        echo "New instance created\n";
-    }
-}
--- src/index.php
+++ src/index.php
@@ -2,6 +2,51 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$testObj = new \App\AutoloadingTest\TestClass();
+$customerChannelFile = '../resources/customer-channel.txt';
 
-echo "\nHello world!!!";
+// Check if it's opened correctly.
+$handle = fopen($customerChannelFile, 'r');
+$content = '';
+
+$i = 0;
+$customerTalk = [
+    // start talking   // stop talking
+    [0,                null]
+];
+
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+        if (mb_strpos($line, 'silence_start') !== false) {
+            // Process the silence_start line
+            preg_match('/silence_start:\s*([\d.]+)/', $line, $matches);
+            $silenceStartAt = $matches[1];
+
+            $customerTalk[$i][1] = $silenceStartAt;
+        } elseif (strpos($line, 'silence_end') !== false) {
+            preg_match('/silence_end:\s*([\d.]+)/', $line, $matches);
+            $silenceEndAt = $matches[1];
+
+            $customerTalk[] = [
+                $silenceEndAt, null
+            ];
+
+            $i++;
+        } else {
+            // This line doesn't contain either 'silence_start' or 'silence_end'
+            echo "Unknown line: " . $line;
+        }
+
+        // Process the line
+        echo $line . "<br />";
+    }
+
+    fclose($handle);
+} else {
+    echo "Error opening the file";
+}
+
+echo '<pre>';
+var_dump($customerTalk);
+
+$userChannelFile = '../resources/user-channel.txt';
+
