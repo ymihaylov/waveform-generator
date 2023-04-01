e5f0c5b975546ff927469e2a74c571a347d206a0
Yavor Mihaylov <myavor@vmware.com>
2023-04-01 11:14:47 +0300

    Finished the task in the POC meaning - let's do some separation  of concerns and refactoring

--- src/index.php
+++ src/index.php
@@ -3,49 +3,99 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$customerChannelFile = '../resources/customer-channel.txt';
+// - Something which is working with files - file manager?
+// - Something which knows how to extract silence start and silence_end
+// - ResponseClass
+// - Monolog class
+// - MonologEntry class
+// - Error handling
 
-// Check if it's opened correctly.
-$handle = fopen($customerChannelFile, 'r');
-$content = '';
+function reverseSilenceFileToMonologArray($filename) {
+    // TODO: Check if it's opened correctly.
+    $handle = fopen($filename, 'r');
+    $content = '';
 
-$result = [];
+    $result = [];
 
-$silenceStartAt = 0;
-$previousEndSilence = 0;
+    $silenceStartAt = 0;
+    $previousEndSilence = 0;
+    if ($handle) {
+        while (($line = fgets($handle)) !== false) {
+            if (str_contains($line, 'silence_start') !== false) {
+                // Process the silence_start line
+                preg_match('/silence_start:\s*([\d.]+)/', $line, $matches);
+                $silenceStartAt = floatval($matches[1]);
+            } elseif (str_contains($line, 'silence_end')) {
+                preg_match('/silence_end:\s*([\d.]+)/', $line, $matches);
+                $silenceEndAt = floatval($matches[1]);
 
-if ($handle) {
-    while (($line = fgets($handle)) !== false) {
-        if (str_contains($line, 'silence_start') !== false) {
-            // Process the silence_start line
-            preg_match('/silence_start:\s*([\d.]+)/', $line, $matches);
-            $silenceStartAt = $matches[1];
-        } elseif (str_contains($line, 'silence_end')) {
-            preg_match('/silence_end:\s*([\d.]+)/', $line, $matches);
-            $silenceEndAt = $matches[1];
+                $result[] = [$previousEndSilence, $silenceStartAt];
+                $previousEndSilence = $silenceEndAt;
+            } else {
+                // This line doesn't contain either 'silence_start' or 'silence_end'
+                echo "Unknown line: " . $line;
+            }
+        }
 
-            $result[] = [$previousEndSilence, $silenceStartAt];
-            $previousEndSilence = $silenceEndAt;
-        } else {
-            // This line doesn't contain either 'silence_start' or 'silence_end'
-            echo "Unknown line: " . $line;
+        fclose($handle);
+    } else {
+        echo "Error opening the file";
+    }
+
+    return $result;
+}
+function calculateLongestMonologue(array $monologData): float {
+    $longestMonolog = 0;
+
+    foreach ($monologData as $monologEntry) {
+        $current = round($monologEntry[1] - $monologEntry[0], 3);
+
+        if ($current > $longestMonolog) {
+            $longestMonolog = $current;
         }
     }
 
-    fclose($handle);
-} else {
-    echo "Error opening the file";
+    return $longestMonolog;
 }
+function calculateTotalMonologue(array $monologData): float {
+    return array_reduce($monologData, function ($carry, $item) {
+        return round($carry + ($item[1] - $item[0]), 3);
+    }, 0);
+}
+function calculateTalkPercentage($userTotalMonolog, $customerTotalMonolog) {
+    $totalTalkTime = $userTotalMonolog + $customerTotalMonolog;
 
-$longestMonolog = 0;
-foreach ($result as $info) {
-    $current = $info[1] - $info[0];
-    if ($current > $longestMonolog) {
-        $longestMonolog = $current;
+    if ($totalTalkTime == 0) {
+        return 0;
     }
+
+    $talkPercentage = ($userTotalMonolog / $totalTalkTime) * 100;
+
+    return round($talkPercentage, 2);
 }
 
-echo '<pre>';
-var_dump($result);
-
+// User data and manipulation
 $userChannelFile = '../resources/user-channel.txt';
+$userMonologData = reverseSilenceFileToMonologArray($userChannelFile);
+$userLongestMonolog = calculateLongestMonologue($userMonologData);
+$userTotalMonolog = calculateTotalMonologue($userMonologData);
+
+// Customer data and manipulation
+$customerChannelFile = '../resources/customer-channel.txt';
+$customerMonologData = reverseSilenceFileToMonologArray($customerChannelFile);
+$customerLongestMonolog = calculateLongestMonologue($customerMonologData);
+$customerTotalMonolog = calculateTotalMonologue($customerMonologData);
+
+
+$userTalkPercentage = calculateTalkPercentage($userTotalMonolog, $customerTotalMonolog);
+
+header('Content-Type: application/json');
+
+// Convert the array to a JSON string and echo it
+echo json_encode([
+    'longest_user_monologue' => $userLongestMonolog,
+    'longest_customer_monologue' => $customerLongestMonolog,
+    'user_talk_percentage' => $userTalkPercentage,
+    "user" => $userMonologData,
+    "customer" => $customerMonologData,
+]);
